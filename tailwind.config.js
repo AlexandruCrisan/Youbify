a75af2eee82585c21f5bf9f5af1d9e13c0934908
AlexandruCrisan <calex2005cj@gmail.com>
2023-09-25 22:46:22 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: ["./templates/*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                dm_sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
+                rubik: ["Rubik", ...defaultTheme.fontFamily.sans],
+                satisfy: ["Satisfy", ...defaultTheme.fontFamily.sans]
+            },
+            colors: {
+                midnightsky: "#001220",
+                youtube: "#B41313",
+                spotify: "#1DB954"
+            },
+            screens: {
+                "3xl": "2000px"
+            }
+        }
+    },
+    plugins: []
+};
+
